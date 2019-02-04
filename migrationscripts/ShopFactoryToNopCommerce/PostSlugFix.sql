95723e839b748fd57caa0126aa85aeb784d76c8e
Sander van den Hoed <s.vdhoed@gmail.com>
2019-02-04 18:23:03 +0100

    Added PostSlugFIx to fix Slugs with duplicate names.

---
+++ migrationscripts/ShopFactoryToNopCommerce/PostSlugFix.sql
@@ -0,0 +1,8 @@
+﻿USE NOPCOMMERCE_BLANK;
+
+with cte as (
+  select *, rn = row_number() over (partition by Slug order by EntityId)
+  from UrlRecord
+) update cte
+	SET slug = slug + '-' + CAST(rn as NVARCHAR(MAX))
+where rn > 1;
