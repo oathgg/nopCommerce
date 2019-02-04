121b047a02b2d25ad566b5075d44583ea7744cee
Sander van den Hoed <s.vdhoed@gmail.com>
2019-02-04 16:37:31 +0100

    Additional filter on Department and created URL routing with SEO friendly names.

--- migrationscripts/ShopFactoryToNopCommerce/SF_Category.sql
+++ migrationscripts/ShopFactoryToNopCommerce/SF_Category.sql
@@ -1,4 +1,5 @@
-﻿DELETE FROM [NOPCOMMERCE_BLANK]..[Category];
+﻿DELETE FROM [NOPCOMMERCE_BLANK]..[URLRecord] where EntityName = 'Category';
+DELETE FROM [NOPCOMMERCE_BLANK]..[Category];
 SET IDENTITY_INSERT [NOPCOMMERCE_BLANK]..[Category] ON;
 
 with CTE_Category AS
@@ -20,19 +21,19 @@ with CTE_Category AS
 		0							as [PictureId],
 		6							as [PageSize],
 		1							as [AllowCustomersToSelectPageSize],
-		'6, 3, 9'					as [PageSizeOptions],
+		null						as [PageSizeOptions],
 		null						as [PriceRanges],
-		CASE
-			WHEN department2.ParentID != '' THEN 0
-			ELSE 1
-		END							as [ShowOnHomePage],
-		CASE
-			WHEN department2.ParentID != '' THEN 0
-			ELSE 1
-		END							as [IncludeInTopMenu],
+		0							as [ShowOnHomePage],
+		0							as [IncludeInTopMenu],
 		0							as [SubjectToAcl],
 		0							as [LimitedToStores],
-		department.Visible			as [Published],
+		CASE
+			-- Keep the default ParentID
+			WHEN department2.ParentID != '' THEN department.Visible
+			-- This will become the Main category	
+			ELSE department2.Visible
+		END							as [Published],
+		--department.Visible			as [Published],
 		0							as [Deleted],
 		ROW_NUMBER() over (partition by department.ParentID order by department.ObjId)				
 									as [DisplayOrder],
@@ -43,8 +44,10 @@ with CTE_Category AS
 		JOIN departmentLang dlang on dlang.ObjID = department.ObjID
 		JOIN departmentBase department2 on department2.ObjID = department.ParentID
 	where	dlang.Name != ''
-	and		department.ParentID != ''
 	and		dlang.LangName = 'nl'
+	and		department.ParentID != ''
+	-- Default pages by SF
+	and		department.Creation != 0
 ) 
 INSERT INTO [NOPCOMMERCE_BLANK]..[Category]
 	([Id], [Name], [Description], [CategoryTemplateId], [MetaKeywords], [MetaDescription], [MetaTitle], [ParentCategoryId], [PictureId], [PageSize], [AllowCustomersToSelectPageSize], 
@@ -52,6 +55,29 @@ INSERT INTO [NOPCOMMERCE_BLANK]..[Category]
 SELECT [Id], [Name], [Description], [CategoryTemplateId], [MetaKeywords], [MetaDescription], [MetaTitle], [ParentCategoryId], [PictureId], [PageSize], [AllowCustomersToSelectPageSize], 
 		[PageSizeOptions], [PriceRanges], [ShowOnHomePage], [IncludeInTopMenu], [SubjectToAcl], [LimitedToStores], [Published], [Deleted], [DisplayOrder], [CreatedOnUtc], [UpdatedOnUtc]
 FROM [CTE_Category]
+	-- Ignore duplicates.
 	Where DupCount < 2
 
 SET IDENTITY_INSERT [NOPCOMMERCE_BLANK]..[Category] OFF;
+
+-- Adds URL routing to the website
+insert into [NOPCOMMERCE_BLANK]..[URLRecord]
+(EntityId, EntityName, Slug, IsActive, LanguageId)
+select 
+	[Category].Id, 'Category', 
+	-- Replaces any weird characters in the slug as this will be used for the URL.
+	REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(RTRIM([Category].NAME)
+		, ' ', '-')
+		, '/', '')
+		, '=', '-')
+		, ',', '')
+		, '+', '')
+		, '(', '')
+		, ')', '')
+		, '--', '-')
+	, 1, 0
+from [NOPCOMMERCE_BLANK]..[Category]
+	LEFT JOIN [NOPCOMMERCE_BLANK]..[URLRecord] on [URLRecord].EntityId = [Category].Id
+		AND [URLRecord].EntityName = 'Category'
+where	[URLRecord].EntityId IS NULL
+AND		[Category].Published = 1;
