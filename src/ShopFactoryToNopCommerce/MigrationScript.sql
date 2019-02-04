cb0822a43015b7578354a1485fb9f1755bb485d0
Sander van den Hoed <s.vdhoed@gmail.com>
2019-02-04 12:37:58 +0100

    Implemented fix for ParentId where the parent had a weird Parent which we didn't want to include.

--- src/ShopFactoryToNopCommerce/MigrationScript.sql
+++ src/ShopFactoryToNopCommerce/MigrationScript.sql
@@ -102,24 +102,34 @@
 
 use sf;
 
--- Manufacturer
-select * from brandbase;
+-- MANUFACTURER
+select * from brandbase
 
--- Product
-select * from productBase pb
-	JOIN productLang pl on pl.ObjID = pb.ObjID
-where pl.LangName = 'nl'
+-- CATEGORY
+select distinct
+	department.ObjId		as [SF_CategoryId],
+	
+	-- If the ParentId is empty of the Parent then this will become the main Category
+	-- The reason why we do this is because SF has everything under parents which are called 'Index', 'Index 2', 'Index 3'
+	CASE
+		WHEN department2.ParentID != '' THEN department.ParentID -- Indicates that this is still fine as the root
+		ELSE '' -- Reset the ParentId of the Parent, as we don't want 'Index', 'Index 2' as categories.
+	END						as [SF_ParentId],
 
--- Category
-select * from departmentBase db
-	join departmentLang dl on dl.ObjID = db.ObjID
-where ParentID != ''
-	and dl.LangName = 'nl'
+	dlang.Name				as [SF_CategoryName]
+from departmentBase department
+	JOIN departmentLang dlang on dlang.ObjID = department.ObjID
+	JOIN departmentBase department2 on department2.ObjID = department.ParentID
+where	dlang.Name != ''
+and		department.ParentID != ''
+and		dlang.LangName = 'nl'
 
--- FLAT LINE FOR PRODUCT
+-- PRODUCT
 select 
-	plang.Name as [ProductName], 
-	dlang.Name as [CategoryName]
+	product.ObjID		as [ProductId],
+	plang.Name			as [ProductName],
+	dlang.Name			as [CategoryName],
+	department.ParentId as [CategoryParentId]
 from productBase product
 	JOIN productLang plang on plang.ObjID = product.ObjID
 	JOIN Links plink on plink.strTo = product.ObjID
@@ -127,38 +137,3 @@ from productBase product
 	JOIN departmentLang dlang on dlang.ObjID = department.ObjID
 where dlang.LangName = 'nl'
 	and department.ParentID != ''
-
-/*
-	ID			PID	PIDREF	strFrom	strTo	strTo2	LinkData
-	952781668	SL1	5		D960	P8415		
-	952781669	SL1	6		D960	P8416		
-	952781670	SL1	7		D960	P8417		
-	952781671	SL1	8		D960	P8418		
-	952781672	SL1	9		D960	P8419		
-	952781673	SL1	10		D960	P8420		
-	952781674	SL1	11		D960	P8421		
-*/
-
--- Contains the relations from Product to Category
-select * from Links
-	where PID = 'SL1'
-
--- Contains the Category and all sub categories
-select dl.Name, * from departmentBase db
-	join departmentLang dl on dl.ObjID = db.ObjID
-where LangName = 'nl'
-	--and db.objid = 'D-33'
-
--- Contains the Product
-select pl.name, * from productBase pb
-	JOIN productLang pl on pl.ObjID = pb.ObjID
-where pb.objid in 
-(
-	'P8416',
-	'P8415',
-	'P8417',
-	'P8418',
-	'P8419',
-	'P8420',
-	'P8421'
-)
