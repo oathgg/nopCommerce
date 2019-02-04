d9c2a66933b31fff92a90a8dec05ca42a51d7e1c
Sander van den Hoed <s.vdhoed@gmail.com>
2019-02-04 22:45:13 +0100

    Updated FixUrls script

--- migrationscripts/PostScripts/FixUrls.ps1
+++ migrationscripts/PostScripts/FixUrls.ps1
@@ -1,41 +1,69 @@
-﻿$ErrorActionPreference = "STOP";
+﻿cls;
 
-#p81553_Viva-Decor-Pouring-Medium.html
-function Fix-Url ([string] $Url) {
-    Get-Module SQLPS -ErrorAction SilentlyContinue | Remove-Module;
-    Import-Module SQLPS;
+$ErrorActionPreference = "STOP";
+$VerbosePreference = "Continue";
 
-    $id = $url.Split('_')[0]; #p81553
-    
-    # Get the SLUG of the ID.
-    $Type = "";
-    switch ($id[0])
-    {
-        'p' { $Type = "Product"; break; }
-        'd' { $Type = "Category"; break; }
-    }
+Get-Module SQLPS -ErrorAction SilentlyContinue | Remove-Module;
+Import-Module SQLPS;
 
-    $idWithoutType = $id.Substring(1, $id.Length - 1);
-    $Query = "SELECT SLUG FROM [NOPCOMMERCE_BLANK]..[URLRECORD] WHERE ENTITYNAME = '$Type' AND ENTITYID = $idWithoutType";
+function Fix-Id ([string] $Entity, [string] $Id) {
+    Write-Verbose "ID '$Id'";
+
+    [int] $idWithoutType = $Id.Substring(1, $id.Length - 1);
+    $Query = "SELECT SLUG FROM [NOPCOMMERCE_BLANK]..[URLRECORD] WHERE ENTITYNAME = '$Entity' AND ENTITYID = $idWithoutType";
+    Write-Verbose $Query; 
 
     $result = Invoke-Sqlcmd -ServerInstance "(localdb)\mssqllocaldb" -Query $Query -QueryTimeout 65000;
-    $newUrl = "";
-    if ($result -ne $null) {
-        $newUrl = $result.SLUG;
-    } else {
-        Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "DEAD LINK: $url";
+    $newId = $result.SLUG;
+    return $newId;
+}
+
+function Fix-Description ([string] $Entity, [string] $FieldName) {
+    $Query = "SELECT [$FieldName] FROM [NOPCOMMERCE_BLANK]..[$Entity] WHERE [$FieldName] LIKE '%.html%'";
+    $result = Invoke-Sqlcmd -ServerInstance "(localdb)\mssqllocaldb" -Query $Query -QueryTimeout 65000;
+    $strArr = $result | Select-Object -Property $FieldName -ExpandProperty $FieldName;
+
+    foreach ($str in $strArr) {
+        $str = $str | Select-Object -ExpandProperty $FieldName;
+        
+        Write-Verbose "STR: $str";
+
+        $Urls = $str.Split('"') | ? { $_ -imatch ".html" };
+
+        foreach ($url in $Urls) {
+            Write-Verbose "URL: $url";
+            
+            if ($url -imatch "http") {
+                Write-Verbose "Skipping $url";
+                Continue;
+            }
+
+            # Remove .html tag
+            $url = $url.Replace(".html", "");
+
+            # Check if there is a # in the name.
+            if ($url -imatch '#') {
+                $url = $url.Split("#")[1];
+            }
+            if ($url -imatch ':') {
+                $url = $url.Split(":")[1];
+            }
+            if ($url -imatch '_') {
+                $url = $url.Split('_')[0];
+            }
+
+            $newId = Fix-Id -Entity $Entity -Id $url;
+
+            if (![string]::IsNullOrEmpty($newId)) {
+                Write-Host -ForegroundColor Green -BackgroundColor Black -Object "FIXED LINK: $fullUrl -> $newId";
+                $str = $str.Replace($Url, $newId);
+            } else {
+                Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object "DEAD LINK: $url.html";
+            }
+        }
     }
-
-    return $newUrl;
 }
 
-$str = '<p><a href="d3193_Maya_Gold.html">klik hier om naar het artikel te gaan</a></p> <p>&nbsp;</p>';
-$url = $str.Split('"') | ? {$_ -imatch ".html"};
-
-$newUrl = Fix-Url -Url $url;
-if ($newUrl -ne "") {
-    $str = $str.Replace($url, $newUrl);
-    Write-Host -ForegroundColor Green -BackgroundColor Black -Object "New string: $str"
-} else {
-    Write-Host -ForegroundColor Yellow -BackgroundColor Black -Object $str;
-}
+Fix-Description -Entity "Category" -FieldName "Description";
+Fix-Description -Entity "Product" -FieldName "ShortDescription";
+Fix-Description -Entity "Product" -FieldName "FullDescription";
